057f1c988cc3cbc04be61d99b2722923d5b9e2e7
sadouni-khouira <67142763+sadouni-khouira@users.noreply.github.com>
2020-08-26 10:41:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-mine = 100;
-//Ecrivez ici la boucle qui vous permet d'enlever 1 unité d'or de la mine et d'annoncer
-//dans la console combien il reste d'or dans la mine.
+//Les variables
+//nombre
+var nombre = 42;
+var tab = [2, 5, 6, 7, 9];
 
-        while (mine >= 0) {                              
-                             console.log("Il reste " + mine +" unités d'or dans la mine");
-                             mine = mine - 1;
-                          }
+var chaine = " ceci est une chaine de caracteres";
+
+console.log(nombre);
+//tableau
+
+console.log(tab);
+//chaine de caracteres
+
+console.log(chaine);
+//Conditions
+
+//if, else if ,else
+
+if (nombre >= 50) { console.log("super moitmoit"); } else if (nombre <= 40) { console.log("peux mieux faire"); } else console.log("la grande réponse");
+
+//switch
+switch (tab.length) {
+    case 10:
+        console.log("il y a 10 éléments dans le tableau");
+        break;
+    case 5:
+        console.log("il y a 5 éléments dans le tableau");
+        break;
+    case 0:
+        console.log(" le tableau est vide");
+        break;
+    default:
+        console.log("Je ne connais pas le nombre d'éléments du tableau");
+}
+//Boucles
+var compteur = 0;
+//while
+while (compteur < 5) {
+    compteur++;
+    // console.log('la ' + compteur + ' fois');
+    console.log("Messire, on en a gros ." + compteur + ' fois');
+}
+//for
+for (var i = 0; i < tab.length; i++) {
+    console.log('élément n° ' + (i + 1) + ' : ' + tab[i]);
+
+}
