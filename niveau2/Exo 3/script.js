54413a0aa45244135ff707afc6758954fa24a13c
sadouni-khouira <67142763+sadouni-khouira@users.noreply.github.com>
2020-08-26 14:03:02 +0200

    Add files via upload

---
+++ niveau2/Exo 3/script.js
@@ -0,0 +1,8 @@
+function multiplication(a, b) {
+    return a * b
+}
+
+var nbr1 = prompt('saisir un nombre ');
+var nbr2 = prompt('saisir un nombre ');
+var p = multiplication(nbr1, nbr2);
+alert("Le résultat est " + p);
